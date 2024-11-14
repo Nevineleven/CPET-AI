adff3f26e40d629b441bbfc315674378a7e24a57
Nevin Aresh <nevinaresh@Nevins-MacBook-Pro.local>
2024-11-14 15:05:47 -0800

    splined time data

--- src/outcomeParsing.py
+++ src/outcomeParsing.py
@@ -3,5 +3,14 @@ import numpy as np
 
 def preprocess(outcomes_data: pd.DataFrame):
     outcomes_data["Test Date"] = pd.to_datetime(outcomes_data["Test Date"], format='%m/%d/%Y')
-    death_data = outcomes_data[["MRN", "Test Date", "Death"]]
+    death_data = outcomes_data[["MRN", "Test Date", "Death", "death_date"]]
+    death_data.dropna(subset=['Death']).sort_values(by=['MRN', 'death_date'], ascending=[True, False]).drop_duplicates(subset='MRN', keep='first')
     return death_data
+
+def mergeDataOutcome(data_with_mrn: pd.DataFrame,
+                     death_data: pd.DataFrame,
+                     time_in_days: float):
+    time_in_sec = time_in_days * 86400
+    data_and_outcomes = pd.merge(data_with_mrn, death_data, on=['MRN'], how='left')
+    data_and_outcomes['Death'] = ((data_and_outcomes['death_date'] - data_and_outcomes['time_of_test']).dt.total_seconds() <= time_in_sec).astype(int)
+    return data_and_outcomes
